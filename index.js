c8471b8a981a84e7fb5fdfbfec4c3cbbca242b08
Mike Kravtsov <mike4040@me.com>
2020-05-24 15:40:19 -0700

    typo fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * Create a Full Name concatenating First and List Names
- * @param {string} firtsName  - First Name of the user
+ * @param {string} firstName  - First Name of the user
  * @param {string} lastName   - Last Name of the user
  * @return {string}
  */
 const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
-console.log(fullName(10, 20));
+console.log(fullName(10, 'Kravtsov'));
