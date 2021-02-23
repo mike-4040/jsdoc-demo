a09c5768333fc2e983d433503c185a47b253753f
Mike Kravtsov <mike@clickup.com>
2021-02-23 08:11:20 -0800

    cleanup

--- utils.js
+++ utils.js
@@ -1,15 +1,16 @@
-const total = (a, b = []) => {
+function total(a, b = []) {
   if ((!Array.isArray(a), !Array.isArray(b))) {
     throw Error("Params should be Array");
   }
 
-  if (a.some( val => typeof val !== 'number') || b.some( val => typeof val !== 'number')) {
+  if (a.some(val => typeof val !== 'number') || b.some(val => typeof val !== 'number')) {
     throw Error("Arrays can contains only numbers");
   }
 
   const sum_a = a.reduce((s, v) => s + v, 0);
   const sum_b = b.reduce((s, v) => s + v, 0);
+  
   return `The Total is $${sum_a + sum_b}.`;
-};
+}
 
 exports.total = total;
